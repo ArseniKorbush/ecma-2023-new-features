641dc2b7fab5a412abcbf2e997a4541aae7807d0
ArseniKorbush <122732165+ArseniKorbush@users.noreply.github.com>
2023-10-29 23:27:37 +0300

    show the difference between sort and tosorted.js

--- ecmascript-2023/toSorted.js
+++ ecmascript-2023/toSorted.js
@@ -1,19 +1,32 @@
-const initialArray = [1, 2, 3]
+const arr = [1, 3, 2]
 
-const res = initialArray.sort((a, b) => {
+const res1 = arr.sort(() => -1)
+// res1: [2, 3, 1]
+// arr[2, 3, 1]
+
+const res2 = arr.toSorted(() => -1)
+// res2: [3, 2, 1]
+// arr[1, 2, 3]
+
+const initialArray = [1, 2, 3, {name: 'Hackages'}]
+
+const result = initialArray.toSorted(() => -1)
+
+res[0].name = 'Hackages 238'
+// [{name: 'Hackages 238'}, 3, 2, 1]
+// [1, 2, 3, {name: 'Hackages 238'}]
+
+const equal = result = initialArray
+
+
+
+
+const initialArrays = [1, 2, 3]
+
+const res = initialArrays.sort((a, b) => {
     return a - b > 0 ? -1 : -1
 })
 
 // res [2, 3, 1]
-// initialArray [2, 3, 1]
+// initialArrays [2, 3, 1]
 // It's method sort 
-
-const smtArray = [1, 2, 3, {name: 'Hackages'}]
-const sort = smtArray.toSorted(() => -1)
-
-sort[0].name = 'Hackages 238'
-// sort [{name: 'Hackages 238'}, 3, 2, 1]
-// smtArray [1, 2, 3, {name: 'Hackages 238'}]
-
-const equal = sort === smtArray
-// equal: false
